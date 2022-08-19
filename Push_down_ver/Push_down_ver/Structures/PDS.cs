198012da415775e5c36a4ecb17816306d4e30490
guyarb6 <81176667+guyarb6@users.noreply.github.com>
2022-08-19 03:36:43 +0300

    a

--- Push_down_ver/Push_down_ver/Structures/PDS.cs
+++ Push_down_ver/Push_down_ver/Structures/PDS.cs
@@ -6,7 +6,9 @@ namespace Push_down_ver.Structures
 {
     public class PdsNode
     {
-
+        public IDictionary;
+        public HashSet<int> AtomicProp;
+        public bool init;
     }
 
     public class PDS
